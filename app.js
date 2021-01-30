85f4f7397f3d8fc97aa7e91a065dccad9654dbac
szarbartosz <szarbartosz@gmail.com>
2021-01-30 20:58:00 +0100

    enable cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const schedulesRouter = require('./controllers/schedule-endpoints')
 require('express-async-errors')
@@ -17,6 +18,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     logger.error(`error connecting to MongoDB: ${error}`)
   })
 
+app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
