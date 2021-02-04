f58a5c71aea369d7553b107997a702cdb8b9d64a
szarbartosz <szarbartosz@gmail.com>
2021-02-04 03:38:14 +0100

    handle jwt errors

--- controllers/schedule-endpoints.js
+++ controllers/schedule-endpoints.js
@@ -1,6 +1,7 @@
 const schedulesRouter = require('express').Router()
 const Schedule = require('../models/schedule')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 schedulesRouter.get('/', async (request, response) => {
   const schedules = await Schedule.find({}).sort({ deadline: 1 })
@@ -16,10 +17,23 @@ schedulesRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 schedulesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  // eslint-disable-next-line no-undef
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const clippingSchedule = new Schedule({
     object: body.object,
